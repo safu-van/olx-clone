91054a5c73776a78a9580bc9b0f65460e02f37fc
safuvan <contactmesafwan@gmail.com>
2024-09-19 11:10:42 +0530

    Product card component design completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
+import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <NavBar /> */}
       {/* <SignIn /> */}
       {/* <SignUp /> */}
-      <Footer />
+      {/* <Footer /> */}
+      <ProductCard />
     </div>
   );
 }
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div className="p-5">
+      <div className="border border-gray-400 max-w-72 rounded-sm">
+        <div className="w-full p-2">
+          <div className="w-full h-52">
+            <img
+              src="assets/images/img-watch.jpg"
+              alt="Product Image"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <div className="p-4">
+            <div>
+              <span className="text-xl font-extrabold">$ 1000</span>
+            </div>
+            <div>
+              <span className="text-md font-semibold">Product Name</span>
+            </div>
+            <div>
+              <span className="text-sm text-gray-500">Category</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
